758a069dbf2391d954e02b6c5207fdbb6bfe7ffe
d4ryus <w.wackerbauer@yahoo.de>
2015-10-29 17:44:37 +0100

    updated mime-type handling to be more readable

--- src/helper.c
+++ src/helper.c
@@ -12,6 +12,182 @@
 #include "msg.h"
 #include "defines.h"
 
+char *mime_types[][2] = {
+	{ "txt",     "text/plain" },
+	{ "htm",     "text/html" },
+	{ "html",    "text/html" },
+	{ "shtml",   "text/html" },
+	{ "jpeg",    "image/jpeg" },
+	{ "jpg",     "image/jpeg" },
+	{ "jpe",     "image/jpeg" },
+	{ "png",     "image/png" },
+	{ "webm",    "video/webm" },
+	{ "mp4",     "video/mp4" },
+	{ "m4v",     "video/mp4" },
+	{ "m4a",     "audio/mp4" },
+	{ "ogv",     "video/ogg" },
+	{ "ogg",     "audio/ogg" },
+	{ "oga",     "audio/ogg" },
+	{ "gif",     "image/gif" },
+	{ "tar",     "application/x-tar" },
+	{ "gz",      "application/gzip" },
+	{ "zip",     "application/zip" },
+	{ "pdf",     "application/pdf" },
+	{ "mpeg",    "video/mpeg" },
+	{ "mpg",     "video/mpeg" },
+	{ "mpe",     "video/mpeg" },
+	{ "dwg",     "application/acad" },
+	{ "asd",     "application/astound" },
+	{ "asn",     "application/astound" },
+	{ "tsp",     "application/dsptype" },
+	{ "dxf",     "application/dxf" },
+	{ "spl",     "application/futuresplash" },
+	{ "ptlk",    "application/listenup" },
+	{ "hqx",     "application/mac-binhex40" },
+	{ "mbd",     "application/mbedlet" },
+	{ "mif",     "application/mif" },
+	{ "xls",     "application/msexcel" },
+	{ "xla",     "application/msexcel" },
+	{ "hlp",     "application/mshelp" },
+	{ "chm",     "application/mshelp" },
+	{ "ppt",     "application/mspowerpoint" },
+	{ "ppz",     "application/mspowerpoint" },
+	{ "pps",     "application/mspowerpoint" },
+	{ "doc",     "application/msword" },
+	{ "dot",     "application/msword" },
+	{ "bin",     "application/octet-stream" },
+	{ "exe",     "application/octet-stream" },
+	{ "com",     "application/octet-stream" },
+	{ "dll",     "application/octet-stream" },
+	{ "oda",     "application/oda" },
+	{ "ai",      "application/postscript" },
+	{ "eps",     "application/postscript" },
+	{ "rtc",     "application/rtc" },
+	{ "rtf",     "application/rtf" },
+	{ "smp",     "application/studiom" },
+	{ "tbk",     "application/toolbook" },
+	{ "vmd",     "application/vocaltec-media-desc" },
+	{ "vmf",     "application/vocaltec-media-file" },
+	{ "htm",     "application/xhtml+xml" },
+	{ "html",    "application/xhtml+xml" },
+	{ "shtml",   "application/xhtml+xml" },
+	{ "xhtml",   "application/xhtml+xml" },
+	{ "xml",     "application/xml" },
+	{ "bcpio",   "application/x-bcpio" },
+	{ "z",       "application/x-compress" },
+	{ "cpio",    "application/x-cpio" },
+	{ "csh",     "application/x-csh" },
+	{ "dcr",     "application/x-director" },
+	{ "dir",     "application/x-director" },
+	{ "dxr",     "application/x-director" },
+	{ "dvi",     "application/x-dvi" },
+	{ "evy",     "application/x-envoy" },
+	{ "gtar",    "application/x-gtar" },
+	{ "hdf",     "application/x-hdf" },
+	{ "php",     "application/x-httpd-php" },
+	{ "phtml",   "application/x-httpd-php" },
+	{ "js",      "application/x-javascript" },
+	{ "latex",   "application/x-latex" },
+	{ "bin",     "application/x-macbinary" },
+	{ "mif",     "application/x-mif" },
+	{ "nc",      "application/x-netcdf" },
+	{ "cdf",     "application/x-netcdf" },
+	{ "nsc",     "application/x-nschat" },
+	{ "sh",      "application/x-sh" },
+	{ "shar",    "application/x-shar" },
+	{ "swf",     "application/x-shockwave-flash" },
+	{ "cab",     "application/x-shockwave-flash" },
+	{ "spr",     "application/x-sprite" },
+	{ "sprite",  "application/x-sprite" },
+	{ "sit",     "application/x-stuffit" },
+	{ "sca",     "application/x-supercard" },
+	{ "sv4cpio", "application/x-sv4cpio" },
+	{ "sv4crc",  "application/x-sv4crc" },
+	{ "tcl",     "application/x-tcl" },
+	{ "tex",     "application/x-tex" },
+	{ "texinfo", "application/x-texinfo" },
+	{ "texi",    "application/x-texinfo" },
+	{ "t",       "application/x-troff" },
+	{ "tr",      "application/x-troff" },
+	{ "man",     "application/x-troff-man" },
+	{ "troff",   "application/x-troff-man" },
+	{ "me",      "application/x-troff-me" },
+	{ "troff",   "application/x-troff-me" },
+	{ "ms",      "application/x-troff-ms" },
+	{ "troff",   "application/x-troff-ms" },
+	{ "ustar",   "application/x-ustar" },
+	{ "src",     "application/x-wais-source" },
+	{ "au",      "audio/basic" },
+	{ "snd",     "audio/basic" },
+	{ "es",      "audio/echospeech" },
+	{ "tsi",     "audio/tsplayer" },
+	{ "vox",     "audio/voxware" },
+	{ "aif",     "audio/x-aiff" },
+	{ "aiff",    "audio/x-aiff" },
+	{ "aifc",    "audio/x-aiff" },
+	{ "dus",     "audio/x-dspeeh" },
+	{ "cht",     "audio/x-dspeeh" },
+	{ "mid",     "audio/x-midi" },
+	{ "midi",    "audio/x-midi" },
+	{ "mp2",     "audio/x-mpeg" },
+	{ "ram",     "audio/x-pn-realaudio" },
+	{ "ra",      "audio/x-pn-realaudio" },
+	{ "rpm",     "audio/x-pn-realaudio-plugin" },
+	{ "stream",  "audio/x-qt-stream" },
+	{ "wav",     "audio/x-wav" },
+	{ "dwf",     "drawing/x-dwf" },
+	{ "cod",     "image/cis-cod" },
+	{ "ras",     "image/cmu-raster" },
+	{ "fif",     "image/fif" },
+	{ "ief",     "image/ief" },
+	{ "tiff",    "image/tiff" },
+	{ "tif",     "image/tiff" },
+	{ "mcf",     "image/vasa" },
+	{ "wbmp",    "image/vnd.wap.wbmp" },
+	{ "fh4",     "image/x-freehand" },
+	{ "fh5",     "image/x-freehand" },
+	{ "fhc",     "image/x-freehand" },
+	{ "ico",     "image/x-icon" },
+	{ "pnm",     "image/x-portable-anymap" },
+	{ "pbm",     "image/x-portable-bitmap" },
+	{ "pgm",     "image/x-portable-graymap" },
+	{ "ppm",     "image/x-portable-pixmap" },
+	{ "rgb",     "image/x-rgb" },
+	{ "xwd",     "image/x-windowdump" },
+	{ "xbm",     "image/x-xbitmap" },
+	{ "xpm",     "image/x-xpixmap" },
+	{ "wrl",     "model/vrml" },
+	{ "csv",     "text/comma-separated-values" },
+	{ "css",     "text/css" },
+	{ "js",      "text/javascript" },
+	{ "rtx",     "text/richtext" },
+	{ "rtf",     "text/rtf" },
+	{ "tsv",     "text/tab-separated-values" },
+	{ "wml",     "text/vnd.wap.wml" },
+	{ "wmlc",    "application/vnd.wap.wmlc" },
+	{ "wmls",    "text/vnd.wap.wmlscript" },
+	{ "wmlsc",   "application/vnd.wap.wmlscriptc" },
+	{ "xml",     "text/xml" },
+	{ "etx",     "text/x-setext" },
+	{ "sgm",     "text/x-sgml" },
+	{ "sgml",    "text/x-sgml" },
+	{ "talk",    "text/x-speech" },
+	{ "spc",     "text/x-speech" },
+	{ "qt",      "video/quicktime" },
+	{ "mov",     "video/quicktime" },
+	{ "viv",     "video/vnd.vivo" },
+	{ "vivo",    "video/vnd.vivo" },
+	{ "avi",     "video/x-msvideo" },
+	{ "movie",   "video/x-sgi-movie" },
+	{ "vts",     "workbook/formulaone" },
+	{ "vtts",    "workbook/formulaone" },
+	{ "3dmf",    "x-world/x-3dmf" },
+	{ "3dm",     "x-world/x-3dmf" },
+	{ "qd3d",    "x-world/x-3dmf" },
+	{ "qd3",     "x-world/x-3dmf" },
+	{ "wrl",     "x-world/x-vrml" },
+};
+
 /*
  * see globals.h
  */
@@ -356,6 +532,8 @@ char *
 get_content_encoding(const char *file_name)
 {
 	char *type;
+	size_t i;
+	size_t elements;
 
 	type = strrchr(file_name, '.');
 	if (type == NULL || strlen(type) == 1) {
@@ -365,255 +543,14 @@ get_content_encoding(const char *file_name)
 
 	if (type == NULL || !strcmp(type, "")) {
 		return "application/octet-stream";
-	} else if (!strcmp(type, "txt")) {
-		return "text/plain";
-	} else if (!strcmp(type, "htm") || !strcmp(type, "html") || !strcmp(type, "shtml")) {
-		return "text/html";
-	} else if (!strcmp(type, "jpeg") || !strcmp(type, "jpg") || !strcmp(type, "jpe")) {
-		return "image/jpeg";
-	} else if (!strcmp(type, "png")) {
-		return "image/png";
-	} else if (!strcmp(type, "webm")) {
-		return "video/webm";
-	} else if (!strcmp(type, "mp4") || !strcmp(type, "m4v")) {
-		return "video/mp4";
-	} else if (!strcmp(type, "m4a")) {
-		return "audio/mp4";
-	} else if (!strcmp(type, "ogv")) {
-		return "video/ogg";
-	} else if (!strcmp(type, "ogg") || !strcmp(type, "oga")) {
-		return "audio/ogg";
-	} else if (!strcmp(type, "gif")) {
-		return "image/gif";
-	} else if (!strcmp(type, "tar")) {
-		return "application/x-tar";
-	} else if (!strcmp(type, "gz")) {
-		return "application/gzip";
-	} else if (!strcmp(type, "zip")) {
-		return "application/zip";
-	} else if (!strcmp(type, "pdf")) {
-		return "application/pdf";
-	} else if (!strcmp(type, "mpeg") || !strcmp(type, "mpg") || !strcmp(type, "mpe")) {
-		return "video/mpeg";
-	} else if (!strcmp(type, "dwg")) {
-		return "application/acad";
-	} else if (!strcmp(type, "asd") || !strcmp(type, "asn")) {
-		return "application/astound";
-	} else if (!strcmp(type, "tsp")) {
-		return "application/dsptype";
-	} else if (!strcmp(type, "dxf")) {
-		return "application/dxf";
-	} else if (!strcmp(type, "spl")) {
-		return "application/futuresplash";
-	} else if (!strcmp(type, "ptlk")) {
-		return "application/listenup";
-	} else if (!strcmp(type, "hqx")) {
-		return "application/mac-binhex40";
-	} else if (!strcmp(type, "mbd")) {
-		return "application/mbedlet";
-	} else if (!strcmp(type, "mif")) {
-		return "application/mif";
-	} else if (!strcmp(type, "xls") || !strcmp(type, "xla")) {
-		return "application/msexcel";
-	} else if (!strcmp(type, "hlp") || !strcmp(type, "chm")) {
-		return "application/mshelp";
-	} else if (!strcmp(type, "ppt") || !strcmp(type, "ppz") || !strcmp(type, "pps") || !strcmp(type, "pot")) {
-		return "application/mspowerpoint";
-	} else if (!strcmp(type, "doc") || !strcmp(type, "dot")) {
-		return "application/msword";
-	} else if (!strcmp(type, "bin") || !strcmp(type, "exe") || !strcmp(type, "com") || !strcmp(type, "dll") || !strcmp(type, "class")) {
-		return "application/octet-stream";
-	} else if (!strcmp(type, "oda")) {
-		return "application/oda";
-	} else if (!strcmp(type, "ai") || !strcmp(type, "eps") || !strcmp(type, "ps")) {
-		return "application/postscript";
-	} else if (!strcmp(type, "rtc")) {
-		return "application/rtc";
-	} else if (!strcmp(type, "rtf")) {
-		return "application/rtf";
-	} else if (!strcmp(type, "smp")) {
-		return "application/studiom";
-	} else if (!strcmp(type, "tbk")) {
-		return "application/toolbook";
-	} else if (!strcmp(type, "vmd")) {
-		return "application/vocaltec-media-desc";
-	} else if (!strcmp(type, "vmf")) {
-		return "application/vocaltec-media-file";
-	} else if (!strcmp(type, "htm") || !strcmp(type, "html") || !strcmp(type, "shtml") || !strcmp(type, "xhtml")) {
-		return "application/xhtml+xml";
-	} else if (!strcmp(type, "xml")) {
-		return "application/xml";
-	} else if (!strcmp(type, "bcpio")) {
-		return "application/x-bcpio";
-	} else if (!strcmp(type, "z")) {
-		return "application/x-compress";
-	} else if (!strcmp(type, "cpio")) {
-		return "application/x-cpio";
-	} else if (!strcmp(type, "csh")) {
-		return "application/x-csh";
-	} else if (!strcmp(type, "dcr") || !strcmp(type, "dir") || !strcmp(type, "dxr")) {
-		return "application/x-director";
-	} else if (!strcmp(type, "dvi")) {
-		return "application/x-dvi";
-	} else if (!strcmp(type, "evy")) {
-		return "application/x-envoy";
-	} else if (!strcmp(type, "gtar")) {
-		return "application/x-gtar";
-	} else if (!strcmp(type, "hdf")) {
-		return "application/x-hdf";
-	} else if (!strcmp(type, "php") || !strcmp(type, "phtml")) {
-		return "application/x-httpd-php";
-	} else if (!strcmp(type, "js")) {
-		return "application/x-javascript";
-	} else if (!strcmp(type, "latex")) {
-		return "application/x-latex";
-	} else if (!strcmp(type, "bin")) {
-		return "application/x-macbinary";
-	} else if (!strcmp(type, "mif")) {
-		return "application/x-mif";
-	} else if (!strcmp(type, "nc") || !strcmp(type, "cdf")) {
-		return "application/x-netcdf";
-	} else if (!strcmp(type, "nsc")) {
-		return "application/x-nschat";
-	} else if (!strcmp(type, "sh")) {
-		return "application/x-sh";
-	} else if (!strcmp(type, "shar")) {
-		return "application/x-shar";
-	} else if (!strcmp(type, "swf") || !strcmp(type, "cab")) {
-		return "application/x-shockwave-flash";
-	} else if (!strcmp(type, "spr") || !strcmp(type, "sprite")) {
-		return "application/x-sprite";
-	} else if (!strcmp(type, "sit")) {
-		return "application/x-stuffit";
-	} else if (!strcmp(type, "sca")) {
-		return "application/x-supercard";
-	} else if (!strcmp(type, "sv4cpio")) {
-		return "application/x-sv4cpio";
-	} else if (!strcmp(type, "sv4crc")) {
-		return "application/x-sv4crc";
-	} else if (!strcmp(type, "tcl")) {
-		return "application/x-tcl";
-	} else if (!strcmp(type, "tex")) {
-		return "application/x-tex";
-	} else if (!strcmp(type, "texinfo") || !strcmp(type, "texi")) {
-		return "application/x-texinfo";
-	} else if (!strcmp(type, "t") || !strcmp(type, "tr") || !strcmp(type, "roff")) {
-		return "application/x-troff";
-	} else if (!strcmp(type, "man") || !strcmp(type, "troff")) {
-		return "application/x-troff-man";
-	} else if (!strcmp(type, "me") || !strcmp(type, "troff")) {
-		return "application/x-troff-me";
-	} else if (!strcmp(type, "ms") || !strcmp(type, "troff")) {
-		return "application/x-troff-ms";
-	} else if (!strcmp(type, "ustar")) {
-		return "application/x-ustar";
-	} else if (!strcmp(type, "src")) {
-		return "application/x-wais-source";
-	} else if (!strcmp(type, "au") || !strcmp(type, "snd")) {
-		return "audio/basic";
-	} else if (!strcmp(type, "es")) {
-		return "audio/echospeech";
-	} else if (!strcmp(type, "tsi")) {
-		return "audio/tsplayer";
-	} else if (!strcmp(type, "vox")) {
-		return "audio/voxware";
-	} else if (!strcmp(type, "aif") || !strcmp(type, "aiff") || !strcmp(type, "aifc")) {
-		return "audio/x-aiff";
-	} else if (!strcmp(type, "dus") || !strcmp(type, "cht")) {
-		return "audio/x-dspeeh";
-	} else if (!strcmp(type, "mid") || !strcmp(type, "midi")) {
-		return "audio/x-midi";
-	} else if (!strcmp(type, "mp2")) {
-		return "audio/x-mpeg";
-	} else if (!strcmp(type, "ram") || !strcmp(type, "ra")) {
-		return "audio/x-pn-realaudio";
-	} else if (!strcmp(type, "rpm")) {
-		return "audio/x-pn-realaudio-plugin";
-	} else if (!strcmp(type, "stream")) {
-		return "audio/x-qt-stream";
-	} else if (!strcmp(type, "wav")) {
-		return "audio/x-wav";
-	} else if (!strcmp(type, "dwf")) {
-		return "drawing/x-dwf";
-	} else if (!strcmp(type, "cod")) {
-		return "image/cis-cod";
-	} else if (!strcmp(type, "ras")) {
-		return "image/cmu-raster";
-	} else if (!strcmp(type, "fif")) {
-		return "image/fif";
-	} else if (!strcmp(type, "ief")) {
-		return "image/ief";
-	} else if (!strcmp(type, "tiff") || !strcmp(type, "tif")) {
-		return "image/tiff";
-	} else if (!strcmp(type, "mcf")) {
-		return "image/vasa";
-	} else if (!strcmp(type, "wbmp")) {
-		return "image/vnd.wap.wbmp";
-	} else if (!strcmp(type, "fh4") || !strcmp(type, "fh5") || !strcmp(type, "fhc")) {
-		return "image/x-freehand";
-	} else if (!strcmp(type, "ico")) {
-		return "image/x-icon";
-	} else if (!strcmp(type, "pnm")) {
-		return "image/x-portable-anymap";
-	} else if (!strcmp(type, "pbm")) {
-		return "image/x-portable-bitmap";
-	} else if (!strcmp(type, "pgm")) {
-		return "image/x-portable-graymap";
-	} else if (!strcmp(type, "ppm")) {
-		return "image/x-portable-pixmap";
-	} else if (!strcmp(type, "rgb")) {
-		return "image/x-rgb";
-	} else if (!strcmp(type, "xwd")) {
-		return "image/x-windowdump";
-	} else if (!strcmp(type, "xbm")) {
-		return "image/x-xbitmap";
-	} else if (!strcmp(type, "xpm")) {
-		return "image/x-xpixmap";
-	} else if (!strcmp(type, "wrl")) {
-		return "model/vrml";
-	} else if (!strcmp(type, "csv")) {
-		return "text/comma-separated-values";
-	} else if (!strcmp(type, "css")) {
-		return "text/css";
-	} else if (!strcmp(type, "js")) {
-		return "text/javascript";
-	} else if (!strcmp(type, "rtx")) {
-		return "text/richtext";
-	} else if (!strcmp(type, "rtf")) {
-		return "text/rtf";
-	} else if (!strcmp(type, "tsv")) {
-		return "text/tab-separated-values";
-	} else if (!strcmp(type, "wml")) {
-		return "text/vnd.wap.wml";
-	} else if (!strcmp(type, "wmlc")) {
-		return "application/vnd.wap.wmlc";
-	} else if (!strcmp(type, "wmls")) {
-		return "text/vnd.wap.wmlscript";
-	} else if (!strcmp(type, "wmlsc")) {
-		return "application/vnd.wap.wmlscriptc";
-	} else if (!strcmp(type, "xml")) {
-		return "text/xml";
-	} else if (!strcmp(type, "etx")) {
-		return "text/x-setext";
-	} else if (!strcmp(type, "sgm") || !strcmp(type, "sgml")) {
-		return "text/x-sgml";
-	} else if (!strcmp(type, "talk") || !strcmp(type, "spc")) {
-		return "text/x-speech";
-	} else if (!strcmp(type, "qt") || !strcmp(type, "mov")) {
-		return "video/quicktime";
-	} else if (!strcmp(type, "viv") || !strcmp(type, "vivo")) {
-		return "video/vnd.vivo";
-	} else if (!strcmp(type, "avi")) {
-		return "video/x-msvideo";
-	} else if (!strcmp(type, "movie")) {
-		return "video/x-sgi-movie";
-	} else if (!strcmp(type, "vts") || !strcmp(type, "vtts")) {
-		return "workbook/formulaone";
-	} else if (!strcmp(type, "3dmf") || !strcmp(type, "3dm") || !strcmp(type, "qd3d") || !strcmp(type, "qd3")) {
-		return "x-world/x-3dmf";
-	} else if (!strcmp(type, "wrl")) {
-		return "x-world/x-vrml";
-	} else {
-		return "text/plain";
 	}
+
+	elements = sizeof(mime_types) / sizeof(mime_types[0]);
+	for (i = 0; i < elements; i++) {
+		if (!strcmp(type, mime_types[i][0])) {
+			return mime_types[i][1];
+		}
+	}
+
+	return "text/plain";
 }
